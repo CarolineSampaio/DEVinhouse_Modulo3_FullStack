6132ccf264cc2186ea490b33a52423c355ec19c7
Caroline Sampaio <caroline_08022@hotmail.com>
2024-01-30 22:02:34 -0300

    feat: adiciona componente menu ao app - aula 2

---
+++ Semana 2/gerenciamento-pets-frontend/src/components/Menu.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-navigation-drawer permanent>
+    <v-list>
+      <v-list-item
+        prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+        title="Caroline Sampaio"
+        subtitle="carol@gmail.com"
+      ></v-list-item>
+    </v-list>
+
+    <v-divider></v-divider>
+
+    <v-list density="compact" nav>
+      <router-link to="/">
+        <v-list-item prepend-icon="mdi-folder" title="Home" value="myfiles"> </v-list-item>
+      </router-link>
+
+      <router-link to="/pets/novo">
+        <v-list-item prepend-icon="mdi-star" title="Novo pet" value="starred"> </v-list-item>
+      </router-link>
+    </v-list>
+  </v-navigation-drawer>
+</template>
