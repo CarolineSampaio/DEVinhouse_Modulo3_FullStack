25c93a91b29f3a3c91222d722a717fb1f38fb713
Caroline Sampaio <caroline_08022@hotmail.com>
2024-02-11 22:58:53 -0300

    refactor: ajuste url autenticacao - aula 2

--- Semana 3/gerenciamento-pets-frontend/src/services/AuthenticationService.js
+++ Semana 3/gerenciamento-pets-frontend/src/services/AuthenticationService.js
@@ -2,7 +2,7 @@ import api from './api'
 
 class AuthenticationService {
   async login(body) {
-    const response = await api.post('http://127.0.0.1:8000/api/login', body)
+    const response = await api.post('login', body)
     return response.data
   }
 }
