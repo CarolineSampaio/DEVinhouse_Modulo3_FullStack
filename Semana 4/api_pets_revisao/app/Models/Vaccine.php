f5952af64155cba8aab4a996f432438fe9ee36e3
Caroline Sampaio <caroline_08022@hotmail.com>
2024-02-15 20:37:49 -0300

    feat: adiciona status e observações a vacinas - aula 4

--- Semana 4/api_pets_revisao/app/Models/Vaccine.php
+++ Semana 4/api_pets_revisao/app/Models/Vaccine.php
@@ -5,12 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Vaccine extends Model {
+class Vaccine extends Model
+{
     use HasFactory;
 
-    protected $fillable = ['professional_id', 'pet_id', 'dose', 'name'];
+    protected $fillable = ['professional_id', 'pet_id', 'dose', 'name', 'status', 'observations'];
 
-    public function professional() {
+    public function professional()
+    {
         return $this->HasOne(Professional::class, 'id', 'professional_id');
     }
 }
