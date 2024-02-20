4ca02764fd95d0064e155400bdf1af0554c5bcd6
Caroline Sampaio <caroline_08022@hotmail.com>
2024-02-20 15:12:32 -0300

    feat: adiciona funcionalidade de upload com aws

--- Semana 4/api_pets_revisao/routes/api.php
+++ Semana 4/api_pets_revisao/routes/api.php
@@ -17,6 +17,8 @@ Route::post('pets/adocao', [AdoptionController::class, 'store']);
 Route::get('pets/{id}/adocao', [AdoptionController::class, 'show']);
 Route::post('login', [AuthController::class, 'store']);
 
+Route::post('upload', [AdoptionController::class, 'upload']);
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('breeds', [BreedController::class, 'store'])->middleware(['auth:sanctum', 'ability:create-races']);
     Route::get('breeds', [BreedController::class, 'index'])->middleware(['auth:sanctum', 'ability:get-races']);
