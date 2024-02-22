1a5b6cf0d9496a52736d87d00c285a6eae5db2ee
Caroline Sampaio <caroline_08022@hotmail.com>
2024-02-21 23:05:13 -0300

    feat: adiciona factory e testes raças - aula 3

---
+++ Semana 5/api_pets_revisao/tests/Feature/BreedTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Breeds;
+use App\Models\User;
+use Tests\TestCase;
+
+class BreedTest extends TestCase
+{
+    public function test_can_add_new_Breed(): void
+    {
+        $user = User::factory()->create(['profile_id' => 2, 'password' => '12345678']);
+
+        $response = $this->actingAs($user)->post('/api/breeds', ['name' => 'Gato']);
+
+        $response->assertStatus(201);
+        $response->assertJson([
+            'name' => 'Gato',
+            'id' => true,
+            'created_at' => true,
+            'updated_at' => true
+        ]);
+    }
+
+    public function test_cannot_create_with_invalid_name(): void
+    {
+        $user = User::factory()->create(['profile_id' => 2, 'password' => '12345678']);
+
+        $response = $this->actingAs($user)->post('/api/breeds', ['name' => 1]);
+
+        $response->assertStatus(400);
+        $response->assertJson([
+            "message" => "The name field must be a string.",
+            "status" => 400,
+            "errors" => [],
+            "data" => []
+        ]);
+    }
+
+    public function test_cannot_create_without_name(): void
+    {
+        $user = User::factory()->create(['profile_id' => 2, 'password' => '12345678']);
+
+        $response = $this->actingAs($user)->post('/api/breeds', ['name' => " "]);
+
+        $response->assertStatus(400);
+        $response->assertJson([
+            "message" => "The name field is required.",
+            "status" => 400,
+            "errors" => [],
+            "data" => []
+        ]);
+    }
+
+    public function test_user_can_list_all_Breeds()
+    {
+        Breeds::factory(3)->create();
+        // Breeds::create(['name' => 'Gato']);
+        // Breeds::create(['name' => 'Cachorro']);
+
+        $user = User::factory()->create(['profile_id' => 2, 'password' => '12345678']);
+        $response = $this->actingAs($user)->get('/api/breeds');
+
+        $response->assertStatus(200)->assertJsonStructure([
+            '*' => [
+                'created_at',
+                'updated_at',
+                'name',
+                'id'
+            ]
+        ]);
+    }
+}
